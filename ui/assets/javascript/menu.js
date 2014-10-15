70a777cc0d35790c063bf8a4b40eafb39b6b13b1
doojin <dmitry.papka@gmail.com>
2014-10-15 16:37:48 -0300

    Unit tests for menu.js added

--- ui/assets/javascript/menu.js
+++ ui/assets/javascript/menu.js
@@ -2,11 +2,11 @@ var menuItem = $('.menu a.item, .menu .link.item');
 var dropdown = $('.menu .dropdown');
 
 var handler = {
-	activate: function() {
-		if(!$(this).hasClass('dropdown')) {
-			$(this).addClass('active').closest('.ui.menu').find('.item').not($(this)).removeClass('active');
-		}
-	}
+    activate: function () {
+        if (!$(this).hasClass('dropdown')) {
+            $(this).addClass('active').closest('.ui.menu').find('.item').not($(this)).removeClass('active');
+        }
+    }
 };
 
 dropdown.dropdown({on: 'hover'});
