2f17be5653ebd323be1ff0b675a8ed30b831f566
doojin <dmitry.papka@gmail.com>
2014-10-16 01:33:04 -0300

    Updating menu.js

--- ui/assets/javascript/menu.js
+++ ui/assets/javascript/menu.js
@@ -1,13 +1,15 @@
-var menuItem = $('.menu a.item, .menu .link.item');
-var dropdown = $('.menu .dropdown');
+function initMenu() {
+    var menuItem = $('.menu a.item, .menu .link.item');
+    var dropdown = $('.menu .dropdown');
 
-var handler = {
-    activate: function () {
-        if (!$(this).hasClass('dropdown')) {
-            $(this).addClass('active').closest('.ui.menu').find('.item').not($(this)).removeClass('active');
+    var handler = {
+        activate: function () {
+            if (!$(this).hasClass('dropdown')) {
+                $(this).addClass('active').closest('.ui.menu').find('.item').not($(this)).removeClass('active');
+            }
         }
-    }
-};
+    };
 
-dropdown.dropdown({on: 'hover'});
-menuItem.on('click', handler.activate);
+    dropdown.dropdown({on: 'hover'});
+    menuItem.on('click', handler.activate);
+}
