59bf361eee2f352880988efef309f278c2a118c9
doojin <dmitry.papka@gmail.com>
2014-10-16 05:04:00 -0300

    Rebuilding sudoku.js

--- ui/assets/javascript/main.js
+++ ui/assets/javascript/main.js
@@ -1,17 +1,3 @@
 $(document).ready(function() {
-    var tableId = '#sudoku';
-
-    initMenu();
-
-    sudoku.init(tableId);
-
-    // Resizing sudoku field on windows resize
-    $(window).on('resize', function() {
-        sudokuPresenter.resize(tableId)
-    });
-
-    // Changing cell's appearance depending on input value
-    $('#sudoku input').on('input', function() {
-        sudokuPresenter.handleNumberChange(this);
-    });
+    sudoku.init('#sudoku');
 });
--- ui/assets/javascript/sudoku.js
+++ ui/assets/javascript/sudoku.js
@@ -1,28 +1,41 @@
-sudokuPresenter = {
-    handleNumberChange: function(element) {
-        if ($(element).val()) {
-            $(element).css('background-color', '#EEEEEE');
-        } else {
-            $(element).css('background-color', '#FFFFFF');
+var sudoku = {
+
+    table: null,
+
+    // Initializes sudoku field inside the element
+    init: function(element) {
+        // Creating table
+        var sudokuTable = document.createElement('table');
+        this.table = sudokuTable;
+        $(sudokuTable).attr('cellspacing', '0');
+        $(sudokuTable).attr('cellpadding', '0');
+        $(sudokuTable).addClass('sudoku');
+
+        // Adding rows to table
+        for (var i=0; i<9; i++) {
+            var tr = document.createElement('tr');
+
+            // Adding cells to rows
+            for (var j=0; j<9; j++) {
+                var td = document.createElement('td');
+                var input = document.createElement('input');
+                $(input).attr('type', 'text');
+                $(input).attr('data-row', i);
+                $(input).attr('data-col', j);
+                $(td).append(input);
+                $(tr).append(td);
+            }
+
+            $(sudokuTable).append(tr);
         }
+
+        $(element).append(sudokuTable);
+        this.resize();
     },
 
-    resize: function(tableId) {
-        var width = $(tableId).width();
-        $(tableId).height(width);
+    // Makes sudoku field's height equal to width
+    resize: function() {
+        var width = $(this.table).width();
+        $(this.table).css('height', width);
     }
-};
-
-sudoku = {
-	tableId: '',
-
-	init: function(tableId) {
-        this.tableId = tableId;
-        this.clearField();
-		sudokuPresenter.resize(this.tableId);
-	},
-
-    clearField: function() {
-        $(this.tableId + ' input').val('');
-    }
-};
+}
