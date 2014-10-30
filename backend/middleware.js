226a03c678b5ef4e395d2cd74755782be78aa541
doojin <dmitry.papka@gmail.com>
2014-10-30 09:50:28 +0000

    Adding section helper to handlebars middleware; Moving page-specific js scripts to section

--- backend/middleware.js
+++ backend/middleware.js
@@ -4,7 +4,16 @@ var handlebars = require('express-handlebars');
 function apply(app) {
 
 	// Handlebars
-	app.engine('handlebars', handlebars({defaultLayout: 'default'}));
+	app.engine('handlebars', handlebars({
+		defaultLayout: 'default',
+		helpers: {
+			section: function(name, options) {
+				if (!this._sections) this._sections = {};
+				this._sections[name] = options.fn(this);
+				return null;
+			}
+		}
+	}));
 	app.set('view engine', 'handlebars');
 }
 
