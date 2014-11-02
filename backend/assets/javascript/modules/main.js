e3cb14694acc74a6c0af9670aa82e86cd703b527
dmppka <dmppka@dmppka-computer.(none)>
2014-11-02 20:34:19 +0200

    Button 'solve' added

--- backend/assets/javascript/modules/main.js
+++ backend/assets/javascript/modules/main.js
@@ -1,3 +1,43 @@
 $(document).ready(function() {
     var sudoku = new Sudoku('#sudoku');
+
+    $('#solve').on('click', function() {
+        var _self = this;
+        // Don't do anything if button is disabled
+        if ($(this).hasClass('disabled')) return;
+        // Disable button
+        $(this).addClass('disabled');
+        // Getting sudoku cells and sending them to the server
+        var matrix = sudoku.getMatrix();
+        var matrixJson = JSON.stringify(matrix);
+        getSudokuResult(matrixJson, function(result) {
+            processResult(result);
+            $(_self).removeClass('disabled');
+        });
+    });
 });
+
+function getSudokuResult(matrix, callback) {
+    $.ajax({
+        url: '/post-sudoku',
+        type: 'POST',
+        data: {
+            matrix: matrix
+        },
+        success: function(data) {
+            callback(data);
+        },
+        error: function(data) {
+            callback({
+                solved: false,
+                errors: [
+                    'Server error'
+                ]
+            });
+        }
+    });
+}
+
+function processResult(result) {
+    console.log(result);
+}
--- ui/assets/javascript/modules/main.js
+++ ui/assets/javascript/modules/main.js
@@ -1,3 +1,48 @@
 $(document).ready(function() {
     var sudoku = new Sudoku('#sudoku');
+
+    $('#solve').on('click', function() {
+        var _self = this;
+        // Don't do anything if button is disabled
+        if ($(this).hasClass('disabled')) return;
+        // Disable button
+        $(this).addClass('disabled');
+        // Getting sudoku cells and sending them to the server
+        var matrix = sudoku.getMatrix();
+        var matrixJson = JSON.stringify(matrix);
+        getSudokuResult(matrixJson, function(result) {
+            processResult(result);
+            $(_self).removeClass('disabled');
+        });
+    });
 });
+
+function getSudokuResult(matrix, callback) {
+    $.ajax({
+        url: '/post-sudoku',
+        type: 'POST',
+        data: {
+            matrix: matrix
+        },
+        success: function(data) {
+            callback(data);
+        },
+        error: function(data) {
+            callback({
+                solved: false,
+                errors: [
+                    'Server error'
+                ]
+            });
+        }
+    });
+}
+
+function processResult(result) {
+    if (result.solved) {
+        // Fill sudoku cells with correct values and disable it
+        // Display success message
+    } else {
+        // Display error message
+    }
+}
