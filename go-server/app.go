a8e5280000d4fe03a73ae9d2797a721a1b84424d
dmppka <dmppka@dmppka-computer.(none)>
2014-11-03 11:34:18 +0200

    Go server implementation added

---
+++ go-server/app.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"./helper"
+
+	"github.com/doojin/gosudoku"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/post-sudoku", PostSudokuHandler).Methods("POST")
+
+	http.Handle("/", r)
+	http.ListenAndServe(":8081", nil)
+}
+
+// PostSudokuHandler handles sudoku solve request
+func PostSudokuHandler(w http.ResponseWriter, r *http.Request) {
+	// Getting matrix json from Node.js server
+	stringMatrix := [9][9]string{}
+	matrixJSON := r.FormValue("matrix")
+
+	// Unmarshaling matrix json
+	json.Unmarshal([]byte(matrixJSON), &stringMatrix)
+
+	// Convertin matrix
+	intMatrix := helper.StringMatrixToIntMatrix(stringMatrix)
+
+	// Resolving sudoku
+	sudoku := new(gosudoku.Sudoku)
+	sudoku.Numbers = intMatrix
+	//resolved := sudoku.Resolve()
+
+	// Creating response for Node.js server
+	//response := helper.BuildResponse(resolved, sudoku)
+}
